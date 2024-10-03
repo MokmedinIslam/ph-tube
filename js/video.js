e755003b61680a9f5cbb1f5ffdf77ab7b8c7a0bb
Mokmedin <mokmedini@gmail.com>
2024-10-03 07:31:27 +0600

    card stucture

--- js/video.js
+++ js/video.js
@@ -19,24 +19,24 @@ const loadVideos = () => {
     )
 };
 
-const cardDemo = {
-    "category_id": "1001",
-    "video_id": "aaag",
-    "thumbnail": "https://i.ibb.co/DRxB1Wm/sunris.jpg",
-    "title": "Sunrise Reverie",
-    "authors": [
-        {
-            "profile_picture": "https://i.ibb.co/yQFJ42h/ava.jpg",
-            "profile_name": "Ava Johnson",
-            "verified": false
-        }
-    ],
-    "others": {
-        "views": "1.1K",
-        "posted_date": "16950"
-    },
-    "description": "'Sunrise Reverie' by Ava Johnson takes listeners on a serene journey through tranquil melodies and soft harmonies. With 1.1K views, this track is perfect for morning relaxation or an evening wind-down. Ava's heartfelt lyrics and soothing voice create a sense of peace, making it a go-to for fans seeking calm and inspiration in their musical choices."
-}
+// const cardDemo = {
+//     "category_id": "1001",
+//     "video_id": "aaag",
+//     "thumbnail": "https://i.ibb.co/DRxB1Wm/sunris.jpg",
+//     "title": "Sunrise Reverie",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/yQFJ42h/ava.jpg",
+//             "profile_name": "Ava Johnson",
+//             "verified": false
+//         }
+//     ],
+//     "others": {
+//         "views": "1.1K",
+//         "posted_date": "16950"
+//     },
+//     "description": "'Sunrise Reverie' by Ava Johnson takes listeners on a serene journey through tranquil melodies and soft harmonies. With 1.1K views, this track is perfect for morning relaxation or an evening wind-down. Ava's heartfelt lyrics and soothing voice create a sense of peace, making it a go-to for fans seeking calm and inspiration in their musical choices."
+// };
 
 const displayVideos = (videos) =>{
     const videoContainer = document.getElementById('videos');
@@ -50,11 +50,17 @@ const displayVideos = (videos) =>{
       src=${video.thumbnail} class= "w-full h-full object-cover"
       alt="Shoes" />
     </figure>
-    <div class="px-0 py-3">
-        <h2 class="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
+    <div class="px-0 py-3 flex gap-3">    
+        <div>
+            <img class="w-10 h-10 rounded-full object-cover" src=${video.authors[0].profile_picture} />
+        </div>
+        <div>
+            <h2 class="font-bold">${video.title}</h2>
+            <div class="flex items-center gap-3">
+                <p class="text-gray-400">${video.authors[0].profile_name}</p>
+                <img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"
+            </div>
+            <p></p>
         </div>
     </div>
         `
